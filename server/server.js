c2a0998935a044358ee63176a7c112d83446d05e
sansankarg <sansankarg@gmail.com>
2025-04-22 00:05:12 +0530

    a

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/markdownFiles', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`${process.env.REACT_APP_BACKEND_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('Error connecting to MongoDB', err));
 
